3dce13f3ed152424700682a97afdc91e3d7f9aa4
marcopaglino <marco.paglino@omninecs.com>
2020-04-05 17:12:09 +0200

    fix error on missing library load add verbose parameter add more comments

--- run_analysis.R
+++ run_analysis.R
@@ -19,7 +19,15 @@ loadDataSet <- function(type) {
 }
 
 
-run_analysis <- function() {
+run_analysis <- function(verbose=0) {
+        ## This function load the data sets of the
+        ## Human Activity Recognition Using Smartphones Data Set, extracts
+        ## the measurements on the mean and standard deviation and creates a
+        ## tidy data set with the average of each variable for each activity and each subject.
+        
+        ## load libraries reshape2 and dplyr
+        library(reshape2)
+        library(dplyr,warn.conflicts = F)
         ## load the activity label list and the feature label list
         featureList<<-read.table("./UCI HAR Dataset/features.txt")
         activityList<<-read.table("./UCI HAR Dataset/activity_labels.txt")
@@ -28,14 +36,16 @@ run_analysis <- function() {
         
         # load the test dataset
         MeasureTest=loadDataSet("test")
-        print("Test data set loaded")
+        if (verbose==1) {print("Test data set loaded")}
         # load the training dataset
         MeasureTrain=loadDataSet("train")
-        print("Training data set loaded")
+        if (verbose==1) {print("Training data set loaded")}
         #bind the two datset
         MeasureUCI=rbind(MeasureTrain,MeasureTest)
-        print("Data set merged with dimensions:")
-        print(dim(MeasureUCI))
+        if (verbose==1) {
+                print("Data set merged with dimensions:")
+                print(dim(MeasureUCI))
+        }
         
         # extract feature for mean and standade deviation
         MeasureUCIrestr=MeasureUCI[,grep("subject|activity|[Mm]ean|std",names(MeasureUCI), value=T)]
@@ -43,7 +53,7 @@ run_analysis <- function() {
         # labels the data set with descriptive variable names
         names(MeasureUCIrestr)[3:88]=paste("avg",names(MeasureUCIrestr)[3:88],sep=".")
         
-        print("Creating tidy data set")
+        if (verbose==1) { print("Creating tidy data set") }
         tidyDS=melt(MeasureUCIrestr, id=c("subject","activity")) %>% dcast(subject+activity ~ variable, mean)
         # write tidy data set
         write.table(tidyDS, file="./tidyDS")
